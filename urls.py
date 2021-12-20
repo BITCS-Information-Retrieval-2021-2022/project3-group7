b869e62214ffcd944c83ca8a9262c783498b2556
yc451 <59825241+yc451@users.noreply.github.com>
2021-12-20 10:02:22 +0800

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from hellosite import views
-
-app_name="hellosite"
-urlpatterns = [
-    path('', views.hello, name='hello'),
-    path('search',views.search,name='search'),
-    path('display',views.display,name='display')
-]
