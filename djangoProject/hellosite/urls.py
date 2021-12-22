c60c3893020b85b3a2a2f55792efe94812ea9a90
Maydaytyh <maydaytyh@gmail.com>
2021-12-23 00:06:19 +0800

    submit first page

--- djangoProject/hellosite/urls.py
+++ djangoProject/hellosite/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path,re_path
 from hellosite import views
-
+from hellosite.views import *
 app_name="hellosite"
 urlpatterns = [
     path('', views.hello, name='hello'),
     path('search',views.search,name='search'),
-    path('display',views.display,name='display')
+    re_path(r'^display',views.display,name='display'),
+    # path('graph',views.graph,name='display'),
+    path(r'graph/<int:ID>', graph)
 ]
--- djangoProject/hellosite/views.py
+++ djangoProject/hellosite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 from .models import User
 # Create your views here.
 def search(request):
@@ -9,9 +10,34 @@ def hello(request):
     return render(request,'begin/index.html')
     
 def display(request):
-    return render(request,'begin/display.html')
+    m=request.GET['wd']
+    # m=m.split(" ")
+    type=request.GET['type']
+    start=request.GET['start']
+    # print(type)
+    post={}
+    post["type"]="RetrievalPapers"
+    post["query"]=m;
+    if type:
+        post["sortByValue"]=1
+    else:
+        post["sortByValue"]=0
+    post["start"]=start
+    total=10
+    hit=[]
+    hit.append({"Sid":"1","title":"Radio Propagation Characteristics for Short Range Body-Centric Wireless Communications","inCitiationsCount":5,"outCitiationsCount":8})
+    hit.append({"Sid":"2","title":"Reduced oxygen tension helps increase the quality of blastocyst available on D5","inCitiationsCount":3,"outCitiationsCount":4})
+    hit.append({"Sid":"3","title":"Quasi-static and dynamic mechanical properties of commercial-purity tungsten processed by ECAE at low temperatures","inCitiationsCount":2,"outCitiationsCount":5})
+    hit.append({"Sid":"4","title":"Nonlinearities in real exchange rate determination: do African exchange rates follow a random walk?","inCitiationsCount":6,"outCitiationsCount":7})
+    hit.append({"Sid":"5","title":"Multiple-point equalization of room impulse response based on the human perception characteristics","inCitiationsCount":8,"outCitiationsCount":3})
+    hits={}
+    hits["total"]=total
+    hits["hit"]=hit
+    post["hits"]=hits
+    json_post=json.dumps(post)
+    # data=json.load(json_post)
+    data=post
+    return render(request,'begin/display.html',{'total':post["hits"]['total'],'hits':post['hits']['hit'],'query':post['query']})
+def graph(request,ID):
+    return render(request,'begin/graph.html',{'ID':ID})
 
-def users(request):
-    users=User.objects.order_by("username")
-    content={"users":users}
-    return render(request,'begin/users.html',content)
