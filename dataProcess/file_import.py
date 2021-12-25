c9462393583f596c40320a55342e5895bac4f976
hjh1012 <86525022+hjh1012@users.noreply.github.com>
2021-12-26 00:34:21 +0800

    Update file_import.py

--- dataProcess/file_import.py
+++ dataProcess/file_import.py
@@ -4,20 +4,17 @@ import jsonlines
 
 myclient = pymongo.MongoClient("mongodb://182.92.1.145:27017/")
 mydb = myclient["InformationRetrieval"]
-mycol = mydb["citation_test"]
+mycol = mydb["citation_es"]
 
-path = 'E:/IRdata/1-100_1/'
+path = 'E:/IRdata/'
 
 files = os.listdir(path)
-print(len(files))
 
-for i in range(40, len(files)):
-    n = 0
+for i in range(0, len(files)):
 
     f = files[i]
     file = path + f
     print(file)
     fr = jsonlines.open(file, 'r')
     mycol.insert_many(fr, ordered=False, bypass_document_validation=True)
-    print(i)
 
