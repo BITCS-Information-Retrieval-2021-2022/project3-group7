9fcd0f22d8e861d4550f1a6e716d8e38652b3007
Maydaytyh <maydaytyh@gmail.com>
2021-12-26 15:44:16 +0800

    submit frontpage final

--- djangoProject/hellosite/urls.py
+++ djangoProject/hellosite/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path,re_path
 from hellosite import views
 from hellosite.views import *
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 app_name="hellosite"
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path('', views.search, name='search'),
     path('search',views.search,name='search'),
     re_path(r'^display',views.display,name='display'),
     # path('graph',views.graph,name='display'),
-    path(r'graph/<str:title>', views.graph,name="graph")
+    path(r'graph/<str:sid>', views.graph,name="graph"),
+    url('sub',views.sub,name='sub')
 ]
--- djangoProject/hellosite/views.py
+++ djangoProject/hellosite/views.py
@@ -2,52 +2,67 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from .models import User
+import requests
+
 # Create your views here.
 def search(request):
     return render(request, 'begin/search.html')
 
-def hello(request):
-    return render(request,'begin/index.html')
-    
 def display(request):
-    m=request.GET['wd']
+    m = request.GET['wd']
     # m=m.split(" ")
-    type=request.GET['type']
-    start=request.GET['start']
+    type = request.GET['type']
+    start = request.GET['start']
+    type=int(type)
     # print(type)
-    post={}
-    post["type"]="RetrievalPapers"
-    post["query"]=m;
-    if type:
-        post["sortByValue"]=1
-    else:
-        post["sortByValue"]=0
-    post["start"]=start
-    if int(start)<10:
-        post["previous"]=0
-    else:
-        post["previous"]=int(start)-10
-    post["next"]=int(start)+10
-    total=10
-    hit=[]
-    hit.append({"Sid":"1","title":"Radio Propagation Characteristics for Short Range Body-Centric Wireless Communications","inCitiationsCount":5,"outCitiationsCount":8})
-    hit.append({"Sid":"2","title":"Reduced oxygen tension helps increase the quality of blastocyst available on D5","inCitiationsCount":3,"outCitiationsCount":4})
-    hit.append({"Sid":"3","title":"Quasi-static and dynamic mechanical properties of commercial-purity tungsten processed by ECAE at low temperatures","inCitiationsCount":2,"outCitiationsCount":5})
-    hit.append({"Sid":"4","title":"Nonlinearities in real exchange rate determination: do African exchange rates follow a random walk?","inCitiationsCount":6,"outCitiationsCount":7})
-    hit.append({"Sid":"5","title":"Multiple-point equalization of room impulse response based on the human perception characteristics","inCitiationsCount":8,"outCitiationsCount":3})
-    hits={}
-    hits["total"]=total
-    hits["hit"]=hit
-    post["hits"]=hits
-    json_post=json.dumps(post)
-    # data=json.load(json_post)
-    data=post
-    return render(request,'begin/display.html',{'next':post["next"],"previous":post["previous"],'type':'0','start':post["start"],'total':post["hits"]['total'],'hits':post['hits']['hit'],'query':post['query']})
-
-def graph(request,title):
     post = {}
-    print(title)
-    post["type"] = "RetrievalNetwork"
-    post["title"]=title
-    return render(request,'begin/graph.html',{'title':title})
+    post["type"] = "RetrievalPapers"
+    post["query"] = m
+    if type:
+        post["sortByValue"] = 1
+    else:
+        post["sortByValue"] = 0
+    post["start"] = start
+    if int(start) < 10:
+        post["previous"] = 0
+    else:
+        post["previous"] = int(start) - 10
+    post["next"] = int(start) + 10
+    r = requests.get(url='http://182.92.1.145:8050/query', params=post)
+    data=json.loads(r.text)
+    print(data)
+    return render(request, 'begin/display.html',
+                  {'next': data["next"], "previous": data["previous"], 'type': '0', 'start': data["start"],
+                   'total': data["hits"]['total'], 'hits': data['hits']['hit'][int(data["start"]):int(data["start"])+10], 'query': data['query']})
 
+
+
+def graph(request,sid):
+    post = {}
+    post["type"] = "RetrievalNetwork"
+    post["query"]=sid
+    r = requests.get(url='http://182.92.1.145:8050/networks', params=post)
+    data=json.loads(r.text)
+    print(type(data))
+    print(data)
+    res={}
+    subgraph = data["subgraph"]
+    nodes=subgraph["nodes"]
+    res["subgraph"]=subgraph
+    for node in nodes:
+        res[str(node["id"])]=node
+    f = open(
+        '/Users/melon/Desktop/Postgraduate/Course/IR/HW/project3-group7/djangoProject/hellosite/templates/begin/sub3.json',
+        'w')
+    json.dump(res, f)
+    f.close()
+    return render(request, 'begin/graph.html')
+
+def sub(request):
+    f = open(
+        '/Users/melon/Desktop/Postgraduate/Course/IR/HW/project3-group7/djangoProject/hellosite/templates/begin/sub3.json',
+        'r')
+    content = f.read()
+    a = json.loads(content)
+    print(a["subgraph"].keys())
+    return HttpResponse(json.dumps(a), content_type="application/json")
